4ed3df45f8e9de22f1b3e67581eb12ba65187508
MauriceWa <144155257+MauriceWa@users.noreply.github.com>
2024-03-04 14:22:37 +0100

    wasd

---
+++ src/main/java/org/duckstudios/todoappfinal/ToDoAppFinalApplication.java
@@ -0,0 +1,13 @@
+package org.duckstudios.todoappfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoAppFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/duckstudios/todoappfinal/ToDoAppFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duckstudios.todoappfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
